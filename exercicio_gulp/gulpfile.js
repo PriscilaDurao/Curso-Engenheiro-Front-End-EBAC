5a1d4055660cd7c96a0b118fa18eb6e3a1c3bb1b
Priscila Durão <112986283+PriscilaDurao@users.noreply.github.com>
2026-08-14 14:47:40 -0300

    Projeto estruturado e automacao com Gulp finalizada

--- exercicio_gulp/gulpfile.js
+++ exercicio_gulp/gulpfile.js
@@ -3,28 +3,36 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
-});
 
-gulp.task('imagemin', function () {
-  return gulp.src('src/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'));
-});
+function compilaSass() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+}
 
-gulp.task('uglify', function () {
-  return gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
+function comprimeImagens() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
 
-gulp.task('default', gulp.parallel('sass', 'imagemin', 'uglify'));
+function comprimeJavaScript() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
-gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
-  gulp.watch('src/images/*', gulp.series('imagemin'));
-  gulp.watch('src/js/*.js', gulp.series('uglify'));
-});
+function watchFiles() {
+    gulp.watch('./src/styles/*.scss', gulp.series(compilaSass));
+    gulp.watch('./src/scripts/*.js', gulp.series(comprimeJavaScript));
+    gulp.watch('./src/images/**/*', gulp.series(comprimeImagens));
+}
+
+exports.default = gulp.series(
+    gulp.parallel(compilaSass, comprimeImagens, comprimeJavaScript),
+    watchFiles
+);
+
+exports.sass = compilaSass;
+exports.images = comprimeImagens;
+exports.javascript = comprimeJavaScript;
