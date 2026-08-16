3e52cd71e29c160f74ba41e7902a1cef850ff287
Priscila Durão <112986283+PriscilaDurao@users.noreply.github.com>
2026-08-16 18:46:44 -0300

    exercicio TS

---
+++ exercicio.ts/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplicacao(5, 4));
+console.log(saudacao("Maria"));
